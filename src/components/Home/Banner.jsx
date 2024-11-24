812a156ab9438fcaec19ae4eae0e86e4900b5a61
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-11-24 19:31:06 +0600

    about section

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -32,7 +32,7 @@ const Banner = () => {
 
   return (
     <div className="container mx-auto">
-      <div className="carousel w-full">
+      <div className="carousel w-full mt-12">
         {banners.map((banner, index) => (
           <div
             style={{
@@ -44,14 +44,21 @@ const Banner = () => {
             id={`slide${index + 1}`}
             className="carousel-item relative w-full h-[90vh] bg-cover bg-no-repeat rounded-xl"
           >
-            <div>
-              <h1>Lorem ipsum dolor sit amet.</h1>
+            <div className="h-full w-full flex items-center pl-36">
+              <div className="text-white space-y-6">
+                <h1 className="text-5xl font-bold">{banner.title}</h1>
+                <p className="w-2/3">{banner.description}</p>
+                <button className="btn btn-primary mr-4">Discover More</button>
+                <button className="btn btn-primary btn-outline !text-white">
+                  Latest Project
+                </button>
+              </div>
             </div>
-            <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+            <div className="absolute flex justify-between transform bottom-12 right-12 gap-6">
               <a href={banner.prev} className="btn btn-circle">
                 ❮
               </a>
-              <a href={banner.next} className="btn btn-circle">
+              <a href={banner.next} className="btn btn-circle btn-primary">
                 ❯
               </a>
             </div>
--- src/components/Home/Homepage.jsx
+++ src/components/Home/Homepage.jsx
@@ -1,9 +1,11 @@
+import About from "./About";
 import Banner from "./Banner";
 
 const Homepage = () => {
   return (
-    <div className="h-screen">
+    <div className="">
       <Banner />
+      <About />
     </div>
   );
 };
